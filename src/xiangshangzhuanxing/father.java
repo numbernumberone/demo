7d7b75d976506a1ce17c28ced9a0e3034d65d57f
numbernumberone <782568327@qq.com>
2018-12-28 09:51:14 +0800

    2018年王锐的提交

---
+++ src/xiangshangzhuanxing/father.java
@@ -0,0 +1,30 @@
+package xiangshangzhuanxing;
+
+public class father {
+    public static void main(String[] args) {
+        int [] a = {1,2,3,1,23,12,7,32};
+        for(int i = 0; i<a.length; i++){
+            for(int j=i+1; j<a.length; j++){
+                if(a[i]>a[j]){
+                    int temp = a[i];
+                    a[i] = a[j];
+                    a[j] = temp;
+                }
+            }
+        }
+        for(int i=0;i<a.length; i++){
+            System.out.println(a[i]);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
