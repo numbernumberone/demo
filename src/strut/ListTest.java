3e9fb57a4af6c4add63c3369b07e3a6d2f195a01
numbernumberone <782568327@qq.com>
2019-01-26 16:45:07 +0800

    lainbiaode tijiao

--- src/strut/ListTest.java
+++ src/strut/ListTest.java
@@ -8,6 +8,17 @@ import sun.security.util.Length;
  * */
 public class ListTest {
 
+    Node head = null; // 头节点
+
+    class Node {
+        Node next = null;// 节点的引用，指向下一个节点
+        int data;// 节点的对象，即内容
+
+        public Node(int data) {
+            this.data = data;
+        }
+    }
+
 
     int ListLength(ListNode headNode){
         int length=0;
@@ -19,4 +30,27 @@ public class ListTest {
         return length;
     }
 
+    public void addNode(int d) {
+        Node newNode = new Node(d);// 实例化一个节点
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        Node tmp = head;
+        while (tmp.next != null) {
+            tmp = tmp.next;
+        }
+        tmp.next = newNode;
+    }
+    public static void main(String[] args) {
+        ListTest list = new ListTest();
+        list.addNode(5);
+        list.addNode(3);
+        list.addNode(1);
+        list.addNode(2);
+        list.addNode(55);
+        list.addNode(36);
+        System.out.println("linkLength:" + list.ListLength());
+
+    }
 }
