4970cbca3e55802feb1798c76f21d661e74a72c9
numbernumberone <782568327@qq.com>
2019-02-12 11:24:20 +0800

    scala

--- test_scala/src/HelloWorld.scala
+++ test_scala/src/HelloWorld.scala
@@ -1,8 +1,16 @@
 object HelloWorld {
 
   def main(args: Array[String]) {
-    println("Hello World!");
-  }
+    //val greetStrings = new Array[String](3)
+    val greetStrings: Array[String] = new Array[String](3)
+    greetStrings(0) = "hello"
+    greetStrings(1) = ","
+    greetStrings(2) = "world!\n"
 
+    for(i <- 0 to 2)
+      print(greetStrings(i))
+
+    println("Hello World! 我是王锐");
+  }
 
 }
