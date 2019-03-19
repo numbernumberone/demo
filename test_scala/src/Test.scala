37e537544804fd0fd76c6cdd75bacd1c65f5e88a
franklin <frankin@github.com>
2019-03-19 22:33:06 +0800

    fgdg

--- test_scala/src/Test.scala
+++ test_scala/src/Test.scala
@@ -1,6 +1,18 @@
 
-object Test {
 
-  println(StrUtils.randStr(64))
+class ChecksumAccumulator {
+  var sum = 0
+}
+
+
+object Test {
+  def main(args: Array[String]): Unit = {
+
+    val acc = new ChecksumAccumulator
+    val csa = new ChecksumAccumulator
+    acc.sum = 7
+    println(acc.sum)
+    println(csa.sum)
+  }
 
 }
