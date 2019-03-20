0d7ccf6faa9027b86c7202b1e5348fd32a5cb867
wangrui <wangrui@metrodata.cn>
2019-03-20 17:45:53 +0800

    change begin

--- test_scala/src/Test.scala
+++ test_scala/src/Test.scala
@@ -1,18 +1,30 @@
-
-
-class ChecksumAccumulator {
-  var sum = 0
-}
-
-
-object Test {
-  def main(args: Array[String]): Unit = {
-
-    val acc = new ChecksumAccumulator
-    val csa = new ChecksumAccumulator
-    acc.sum = 7
-    println(acc.sum)
-    println(csa.sum)
-  }
-
-}
+//
+//import com.sun.corba.se.impl.javax.rmi.CORBA.Util
+//
+//class ChecksumAccumulator {
+//  private var total = 0
+//  var sum = 0
+//
+//  def add (b : Byte) : Unit = {
+//    total += b
+//  }
+//
+//  def checksum() : Int = {
+//    return ~(sum & 0xFF) + 1
+//  }
+//}
+//
+//
+//object Test {
+//  def main(args: Array[String]): Unit = {
+//
+//    val acc = new ChecksumAccumulator
+//    val csa = new ChecksumAccumulator
+//    acc.sum = 7
+//    acc.add(1)
+//    val a = acc.checksum()
+//    println(a)
+////    println(csa.sum)
+//  }
+//
+//}
--- untitled/src/test.scala
+++ untitled/src/test.scala
@@ -1,7 +1,20 @@
+import scala.collection.mutable.ArrayBuffer
+
 object testdemo1 {
   def main(args: Array[String]): Unit = {
     val s = Array("sfd","sdfsdf")
-//    println(s)
-    println( s(0))
+    val s1 = ArrayBuffer[Int]()
+    val s2 = new ArrayBuffer[Int]()
+
+    s1 += 1
+
+    val num = s1.toArray
+
+    println(0 until 10)
+
+
+    for(i <- 0 until 10)
+      println(i + ": " )
+
   }
 }
